1adf10e21cb2132a2bf9e508ad132564e0cf4a54
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-08-18 11:43:51 +1000

    Initial skeleton, styles and header

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require_once('./inc/header.php');
+?>  
+
+<section id="hero">
+    <h1>My-Com provide high quality refurbished and new computer components at competitive prices</h1>
+    <img src="./public/images/slider3.jpg" alt="Inside of a computer">
+</section>
+
+</body>
+</html>
