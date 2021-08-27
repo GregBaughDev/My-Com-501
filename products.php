6bc0ed299d216a7481f9221b013084588e52ec6f
Greg <80447308+GregBDesign@users.noreply.github.com>
2021-08-27 11:16:10 +1000

    Updates product display styles

--- products.php
+++ products.php
@@ -18,37 +18,43 @@
         <h1>Products</h1>
         <h2>Current products</h2>
     </div>
-    <!-- <div class="flex"> -->
+    <div class="flex-row product-search">
+        <div>
+            <form action="" type="GET">
+                <label for="search">Item Search:</label>
+                <input name="search" id="search" type="text">
+                <input type="submit" value="Search">
+            </form>
+        </div>
+        <div>
+            <label for="">Filter Products:</label>
+            <select name="filter" id="filter">
+                <option value="">Filter Products</option>
+                <option value="current">Current Stock</option>
+                <option value="previous">Previous Stock</option>
+            </select>
+        </div>
+    </div>
     <div class="product-main">
-        <!-- <div class="flex product-row"> -->
         <div class="grid">
             <?php for($i = 0; $i < count($retrieveItems); $i++) { ?>
-                <div class="grid-product">
+                <div class="flex-product">
                     <h2><?php echo $retrieveItems[$i]['name'] ?></h2>
-                    <h3>$<?php echo $retrieveItems[$i]['price'] ?></h3>
-                    <h4><?php echo $retrieveItems[$i]['status'] ?></h4>
-                    <h5><?php echo $retrieveItems[$i]['condition'] ?></h5>
+                    <div class="flex-row product-information">
+                        <img src="./public/img/test.jpg" alt="Computer monitor">
+                        <div class="flex-column product-information-rt">
+                            <h3>$<?php echo $retrieveItems[$i]['price'] ?></h3>
+                            <h4><?php echo $retrieveItems[$i]['status'] ?></h4>
+                            <h5><?php echo $retrieveItems[$i]['condition'] ?></h5>
+                        </div>
+                    </div>
                     <p><?php echo $retrieveItems[$i]['description'] ?></p>
-                    <p><?php echo $retrieveItems[$i]['category'] ?></p>
-                    <p><?php echo $retrieveItems[$i]['manufacturer'] ?></p>
+                    <div class="flex-row product-information-bttm">
+                        <h6>Category: <?php echo $retrieveItems[$i]['category'] ?></h6>
+                        <h6>Brand: <?php echo $retrieveItems[$i]['manufacturer'] ?></h6>
+                    </div>
                 </div>
             <?php } ?>
-        </div>
-        <div class="flex product-row">
-            <div class="product-placeholder">
-            </div>
-            <div class="product-placeholder">
-            </div>
-            <div class="product-placeholder">
-            </div>
-        </div>
-        <div class="flex product-row">
-            <div class="product-placeholder">
-            </div>
-            <div class="product-placeholder">
-            </div>
-            <div class="product-placeholder">
-            </div>
         </div>
     </div>
 </section>
